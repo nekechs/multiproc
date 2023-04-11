7bf852d3bb894974e75b0f35c0bd8e6088916579
RonikKapadia <97857847+RonikKapadia@users.noreply.github.com>
2023-04-11 18:25:39 -0400

    Updated bfs_solver_signal.h header file

--- include/bfs_solver_signal.h
+++ include/bfs_solver_signal.h
@@ -1,6 +1,6 @@
 #ifndef _BFS_SOLVER_SIGNAL_H_
 #define _BFS_SOLVER_SIGNAL_H_
 
-int solve_bfs_signal(char *filename, int *max, double *avg, int *keys, int H, int pn, FILE *outfile);
+int solve_bfs_signal(char *filename, int *max, double *avg, int *keys, int pn, int H, FILE *outfile);
 
 #endif
